95236812c133fc3c944509a12ac151acb9c60a79
Bradley Wogsland <bradley@wogsland.org>
2017-03-14 18:58:43 -0500

    11.1

---
+++ dict-search.py
@@ -0,0 +1,12 @@
+moby = open('words.txt')
+searchable = dict()
+for line in moby:
+    searchable[line.strip()] = line.strip()
+
+def search_dict (word, to_search):
+    return word in to_search
+
+if search_dict('fuck', searchable):
+    print 'Swear word found.'
+else:
+    print 'This dict is PG.'
